4f4a7f02c0fe2ddf9115f3840c53e1c6b1beb43b
Sudhanshu Shekhar Pandey <115818001+Sudhanshu-shekharPandey@users.noreply.github.com>
2023-02-11 23:00:32 +0530

    Update day2_6.py

--- day2_6.py
+++ day2_6.py
@@ -1,7 +1,7 @@
 j=4
 for i in range(1,5):
     print(j*" ",end="")
-    for k in range(i,0,-1):
+    for k in range(i,i+i):
         print(k,end="")
     for l in range(2,i+1):
         print(l,end='')
