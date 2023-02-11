86e7eaeab2c929935b5de940721f3295678f38e9
Sudhanshu Shekhar Pandey <115818001+Sudhanshu-shekharPandey@users.noreply.github.com>
2023-02-11 22:29:44 +0530

    Update day2_1.py

--- day2_1.py
+++ day2_1.py
@@ -3,7 +3,7 @@ j=1
 for i in range(1,5):
     if(c!=j):
         for j in range(i,i+i):
-            print(j,end=" ")
+            print(j,end="")
             c=j
             j=j+1   
     print(" ") 
